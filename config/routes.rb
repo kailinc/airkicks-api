e267e1de5af82d0af0ad68b8b35eea7b3ce1df3c
kailinc <kailinc@bu.edu>
2017-07-21 15:17:38 -0400

    Organized Routes.rb with comments grouped each route added comments for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
 
-  # connect shoe collection routes
+  # CONNECT SHOE COLLECTIONS
   resources :connect_shoe_collections, only: [:index, :show, :create, :destroy]
-  get '/user-collections/:id' => 'users#usercollections'
+
 
   # COLLECTION routes
   resources :collections, only: [:index, :show, :create, :destroy, :update]
 
   # SHOE ROUTES
   resources :shoes, only: [:index, :show, :destroy, :create, :update]
-  # USER ROUTES
+
+  # EXAMPLE ROUTES
   resources :examples, except: [:new, :edit]
+
+  # USER ROUTES
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   get '/user-shoes/:id' => 'users#usershoes'
+  get '/user-collections/:id' => 'users#usercollections'
   resources :users, only: [:index, :show]
 
 end
