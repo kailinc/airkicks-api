bb4ec309a5977c9962102ca7ae2e25b8a6b79293
kailinc <kailinc@bu.edu>
2017-08-13 15:06:26 -0400

    Created scripts for deleting connects table
    - user auth works

--- scripts/delete-connect-shoe-collection.sh
+++ scripts/delete-connect-shoe-collection.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-ID='8'
+ID='3'
+TOKEN='BAhJIiU5NDJlNmU1Tk0YjU0MjI4OTUxYTU2OGQ1MjliNTAxZgY6BkVG--5ecdd9e2d14522e46d8721c2adcc3469de7d6e63'
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/connect_shoe_collections"
+URL_PATH="/connects"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request DELETE \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN"
 
 echo
